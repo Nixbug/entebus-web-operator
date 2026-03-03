911af7901ac33915d3e14c235d6d62f91209aa36
Almehaboob <allualmehboob10@gmail.com>
2026-03-03 10:50:07 +0530

    Remove search debounce delay constant from constants file

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,5 +2,3 @@
 export const DESKTOP_BREAKPOINT: number = 1024;
 export const MOBILE_BREAKPOINT: number = 768;
 
-//-- Debounce delay for map search input (in milliseconds) --
-export const SEARCH_DEBOUNCE_DELAY: number = 300;
